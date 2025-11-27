be032cb9bc1804733fecfbf6efefdc3d9fea29fd
zei-abhi-test <usetoshiba27@gmail.com>
2025-11-27 23:54:33 +0530

    MiniProjects

--- Q-6/my-react-q6/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Counter from "./Counter";
-
-export default function App() {
-  return(
-    <div>
-      <Counter />
-    </div>
-  )
-}
--- Q-8/q-8/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import FormApp from "./Form";
-
-export default function App() {
-  return (
-    <div>
-      <FormApp />
-    </div>
-  )
-}
